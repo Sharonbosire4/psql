2f7616a01652c247aa408f7213f56e7f168c52cf
= <=>
2020-05-03 23:31:54 +0200

    - question 9 - fix quatation mark

--- assignment.sql
+++ assignment.sql
@@ -12,7 +12,7 @@ case when ((sum(case when amount > 0 THEN amount else 0 END)) > ((sum(case when 
 else 'depositer' END as agent_status, count(*)from agent_transactions WHERE when_created  between (now()  - '1 WEEK'::INTERVAL) and now()
 
 
-select agents.city,  count(amount)  from agent_transactions “atx volume city summary" inner join agents on agents.agent_id=agent_transactions.agent_id 
+select agents.city,  count(amount)  from agent_transactions “atx volume city summary inner join agents on agents.agent_id=agent_transactions.agent_id 
 where when_created  > current_date -interval '7 days'  GROUP BY agents.city ;
 
 
@@ -21,3 +21,6 @@ SELECT agents.city, agents.country, SUM (agent_transaction_amount) AS transactio
 
 
 SELECT agents.city, SUM (transfers.send_amount_scalar) AS transaction_scalar, transfers.kind AS transfer_kind FROM transfer, agents, agents_transactions WHERE (agents_transactions.when_created >= DATE_TRUNC ('week', CURRENT_TIMESTAMP - interval '1 week'))
+
+
+SELECT agents.city, SUM (transfers.send_amount_scalar) AS transaction_scalar, COUNT (agent_transactions.amount) AS transaction_volume, transfers.kind AS transfer_kind, COUNT (DISTINCT (transfer_id) AS unique_SENDERS FROM transfers, agents, agent_transactions WHERE (agent_transactions.when_created >= DATE_TRUCN ('week', CURRENT_TIMESTAMP - interval '1 week')) AND agent_transactions.when_created > DATE_TRUNC ('week', CURRENT_TIMESTAMP) GROUP BY country, kind, city
