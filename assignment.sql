2148f6000300efba10353c4ee538e020c27a5925
= <=>
2020-05-03 23:33:09 +0200

    question 10

--- assignment.sql
+++ assignment.sql
@@ -24,3 +24,7 @@ SELECT agents.city, SUM (transfers.send_amount_scalar) AS transaction_scalar, tr
 
 
 SELECT agents.city, SUM (transfers.send_amount_scalar) AS transaction_scalar, COUNT (agent_transactions.amount) AS transaction_volume, transfers.kind AS transfer_kind, COUNT (DISTINCT (transfer_id) AS unique_SENDERS FROM transfers, agents, agent_transactions WHERE (agent_transactions.when_created >= DATE_TRUCN ('week', CURRENT_TIMESTAMP - interval '1 week')) AND agent_transactions.when_created > DATE_TRUNC ('week', CURRENT_TIMESTAMP) GROUP BY country, kind, city
+
+
+SELECT transfers. source_wallet_id, transfers.send_amount_scalar FROM transfers
+ WHERE send_amount_currency * 'CFA' AND send_amount_scalar >10000000;
