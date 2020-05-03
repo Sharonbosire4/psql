d2cce34a8dc9dc60582b7dc4c691cab0a3cb40c9
= <=>
2020-05-03 23:26:53 +0200

    question four

--- assignment.sql
+++ assignment.sql
@@ -4,3 +4,5 @@ select  count(*) from users;
 select count(*) from transfers where send_amount_currency = 'CFA';
 
 select count(distinct u_id) from transfers; 
+
+select count(*), extract(months from when_created) as month from transfers where extract(years from when_created) = 2018 GROUP BY extract(months from when_created);
